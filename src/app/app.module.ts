d8faca3381c492df7341337518c9169d90f6f114
marcelo.grion <marcelo.grion@globallogic.com>
2022-05-24 09:53:55 -0300

    icorporacion de navbar list-usuarios y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { ListUsuariosComponent } from './dashboard/list-usuarios/list-usuarios.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    ListUsuariosComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
