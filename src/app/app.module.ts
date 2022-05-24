5d310f82d954d0ef8f89c6f733dd1a4567046f91
marcelo.grion <marcelo.grion@globallogic.com>
2022-05-24 19:21:47 -0300

    ruteos parte 1 y estilos, fin del dia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './dashboard/footer/footer.component';
 import { NavbarComponent } from './dashboard/navbar/navbar.component';
@@ -16,7 +16,7 @@ import { LayoutComponent } from './layout/layout.component';
     FooterComponent,
     NavbarComponent,
     ListUsuariosComponent,
-    LayoutComponent
+    LayoutComponent 
   ],
   imports: [
     BrowserModule,
